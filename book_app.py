5f2473838edc71aad957facb964060422e38566e
SileiHuo <shuo.2020@outlook.com>
2022-12-29 15:28:09 -0800

    update recommendations

--- book_app.py
+++ book_app.py
@@ -8,15 +8,11 @@ import streamlit as st
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
 
-st.title("Children's Book Recommendation")
-
 @st.cache
 def load_image(onlineurl):
     image = Image.open(requests.get(onlineurl, stream=True).raw)
     return image
 
-image = load_image('https://images.unsplash.com/photo-1472162072942-cd5147eb3902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80')
-st.image(image, caption='Photo by Ben White on Unsplash')
 
 @st.cache
 def load_data(path):
@@ -34,20 +30,16 @@ def load_matrix(path):
 desfull_tfidf_matrix = load_matrix('data/desfull_tfidf_matrix.npz')
 book_user_matrix = load_matrix('data/book_user_matrix.npz')
 
-# def content_similarity():
-#     tfidf_matrix = sparse.load_npz('data/desfull_tfidf_matrix.npz')
-#     similarities_tfidf = cosine_similarity(tfidf_matrix, dense_output=False)
-#     return similarities_tfidf
-# # Content similarity
-# similarities_tfidf = content_similarity()
+### Show entered books
+def show_book(title):
+    title = str(title)
+    if df_book['title'].str.contains(title, case=False).any():
+        book_index = df_book[df_book['title'].str.contains(title, case=False)].sort_values(by='ratings_count',ascending=False).index[0]
+        url = df_book.iloc[book_index]['image_url']
+        st.image(url, width =180, use_column_width=False, caption=df_book.iloc[book_index]['title'])
+    else:
+        st.write('Sorry, book not found. Instead, below are some recommended popular books.')
 
-# @st.cache
-# def collab_similarity():
-#     book_user_matrix = sparse.load_npz('data/book_user_matrix.npz')
-#     similarities_item = cosine_similarity(book_user_matrix, dense_output=False)
-#     return similarities_item
-# # Collaborative similarity
-# similarities_item = collab_similarity()
 
 ### Content Based Recommendation
 def content_recommender(title, vote_threshold=100):
@@ -58,37 +50,28 @@ def content_recommender(title, vote_threshold=100):
         # locate the book's index
         book_index = df_book[df_book['title'].str.contains(title, case=False)].sort_values(by='ratings_count',ascending=False).index[0]
         # create a book dataframe with key features
-        sim_books = pd.DataFrame({'book': df_book['title'], 
+        sim_books = pd.DataFrame({'title': df_book['title'], 
                                'similarity': cosine_similarity(desfull_tfidf_matrix[book_index], desfull_tfidf_matrix).squeeze(),
                            'rating count': df_book['ratings_count'],
                            'rating': df_book['average_rating'],
-                           'url': df_book['image_url']})
+                           'image_url': df_book['image_url']})
         # select top 5 books with highest similarity score
-        top_books = sim_books[sim_books['rating count'] > vote_threshold].sort_values(by='similarity', ascending=False).head(5)
+        top_books = sim_books[sim_books['rating count'] > vote_threshold].sort_values(by='similarity', ascending=False).iloc[1:6]
+        return top_books
         # plot the book cover
-        fig, axs = plt.subplots(1, 5,figsize=(12,3))
-        # fig.suptitle('You may also like these books', size = 12, color='indigo')
-        
-        for i in range(len(top_books['book'].tolist())):
-            url = top_books.iloc[i]['url']
-            im = Image.open(requests.get(url, stream=True).raw)
-            axs[i].imshow(im)
-            axs[i].axis("off")
-            axs[i].set_title('Rating: {}'.format(top_books.iloc[i]['rating']),y=-0.18,color="indigo",fontsize=10)
+        # cols= st.columns(5)
+        # for url, title, col in zip(top_books['image_url'], top_books['title'], cols[0:]):
+        #     with col:
+        #         st.image(url, use_column_width=True, caption=title)   
     else:
         # if book title does not exist, recommend top rated books
-        pop_df = df_book[df_book['ratings_count'] > 50000][['title', 'average_rating', 'image_url']].sort_values(by='average_rating', ascending=False).head(5)
+        top_books = df_book[df_book['ratings_count'] > 50000].sort_values(by='average_rating', ascending=False).head(5)
+        return top_books
         # plot book cover images
-        fig, axs = plt.subplots(1, 5, figsize=(12,3))
-        fig.suptitle('Can not find the book in this dataset, please check spelling. Recommend popular books for you:', size = 12, color='indigo')
-        
-        for i in range(len(pop_df['title'].tolist())):
-            url = pop_df.iloc[i]['image_url']
-            im = Image.open(requests.get(url, stream=True).raw)
-            axs[i].imshow(im)
-            axs[i].axis("off")
-            axs[i].set_title('Rating: {}'.format(pop_df.iloc[i]['average_rating']),y=-0.18,color="indigo",fontsize=10)
-            
+        # cols= st.columns(5)
+        # for url, title, col in zip(top_books['image_url'], pop_df['title'], cols[0:]):
+        #     with col:
+        #         st.image(url, use_column_width=True, caption=title)
 
 ### User-Item Collaborative Based Recommendation
 def collaborative_recommender(title, vote_threshold=100):
@@ -99,50 +82,80 @@ def collaborative_recommender(title, vote_threshold=100):
         
         book_index = df_book_item[df_book_item['title'].str.contains(title, case=False)].sort_values(by='ratings_count',ascending=False).index[0]
         # calculate cosine similarity based on user-item matrix instead
-        sim_item_df = pd.DataFrame({'book': df_book_item['title'], 
+        sim_item_df = pd.DataFrame({'title': df_book_item['title'], 
                            'similarity': cosine_similarity(book_user_matrix[book_index], book_user_matrix).squeeze(),
                            'rating count': df_book_item['ratings_count'],
                            'rating': df_book_item['average_rating'],
-                           'url': df_book_item['image_url']})
+                           'image_url': df_book_item['image_url']})
         
-        top_books = sim_item_df[sim_item_df['rating count'] > vote_threshold].sort_values(by='similarity', ascending=False).head(5)
-
-        fig, axs = plt.subplots(1, 5,figsize=(12,3))
-        # fig.suptitle('Readers also liked', size = 12, color='indigo')
-        
-        for i in range(len(top_books['book'].tolist())):
-            url = top_books.iloc[i]['url']
-            im = Image.open(requests.get(url, stream=True).raw)
-            axs[i].imshow(im)
-            axs[i].axis("off")
-            axs[i].set_title('Rating: {}'.format(top_books.iloc[i]['rating']),y=-0.18,color="indigo",fontsize=10)
+        top_books = sim_item_df[sim_item_df['rating count'] > vote_threshold].sort_values(by='similarity', ascending=False).iloc[1:6]
+        return top_books
+        # cols= st.columns(5)
+        # for url, title, col in zip(top_books['image_url'], top_books['title'], cols[0:]):
+        #     with col:
+        #         st.image(url, use_column_width=True, caption=title)
     else:
-        pop_df = df_book_item[df_book_item['ratings_count'] > 50000][['title', 'average_rating', 'image_url']].sort_values(by='average_rating', ascending=False).head(5)
+        top_books = df_book_item[df_book_item['ratings_count'] > 50000][['title', 'average_rating', 'image_url']].sort_values(by='average_rating', ascending=False).iloc[5:10]
+        return top_books
+        # cols= st.columns(5)
+        # for url, title, col in zip(pop_df['image_url'], top_books['title'], cols[0:]):
+        #     with col:
+        #         st.image(url, use_column_width=True, caption=title)
+
+### Author Based Recommendation
+def author_recommender(author, vote_threshold=50):
+    
+    author = str(author)
+    
+    if df_book['author_name'].str.contains(author, case=False).any():
+        df_author = df_book[df_book['author_name'].str.contains(author, case=False)]
+        top_books = df_author[df_author['ratings_count'] > vote_threshold].sort_values(by='average_rating', ascending=False).head()
+        cols= st.columns(5)
+        for url, title, col in zip(top_books['image_url'], top_books['title'], cols[0:]):
+            with col:
+                st.image(url, use_column_width=True, caption=title)
+    else:
+        st.write('Sorry, no author found by that name. Please try again.')
+
+### APP Presense
+st.title("Children's Book Recommendation")
+image = load_image('https://images.unsplash.com/photo-1472162072942-cd5147eb3902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80')
+st.image(image, caption='Photo by Ben White on Unsplash')
+
+def main():
+    menu = ['Search by Book Title','Search by Author']
+    choice = st.sidebar.selectbox("Choose recommendation methods below",menu)
+    
+    if choice == 'Search by Book Title':
+        st.subheader('Recommend by Book Title')
+        st.write('Please note that due to limited resources, this dataset does not include a comprehensive list of titles.')
+        book_title = st.text_input('Enter a book you like:', 'the very hungry caterpillar')
+        show_book(book_title)
         
-        fig, axs = plt.subplots(1, 5,figsize=(12,3))
-        fig.suptitle('Can not find the book in this dataset, please check spelling. Recommend popular books for you:', size = 12, color='indigo')
-        
-        for i in range(len(pop_df['title'].tolist())):
-            url = pop_df.iloc[i]['image_url']
-            im = Image.open(requests.get(url, stream=True).raw)
-            axs[i].imshow(im)
-            axs[i].axis("off")
-            axs[i].set_title('Rating: {}'.format(pop_df.iloc[i]['average_rating']),y=-0.18,color="indigo",fontsize=10)
-            
+        ### Content Based
+        st.subheader('You may also like these books:\n (content based recommendation)')  
+        top_books_content=content_recommender(book_title)
+        cols= st.columns(5)
+        for url, title, col in zip(top_books_content['image_url'], top_books_content['title'], cols[0:]):
+            with col:
+                st.image(url, use_column_width=True, caption=title)
+        ### Collaborative Based
+        st.subheader('Readers also liked these books:\n (collaborative based recommendation)')   
+        top_books_collab = collaborative_recommender(book_title)
+        cols=st.columns(5)
+        for url, title, col in zip(top_books_collab['image_url'], top_books_collab['title'], cols[0:]):
+            with col:
+                st.image(url, use_column_width=True, caption=title)
+    
+    else:
+        ### Author Recommendation
+        st.subheader('Recommend by Author')
+        st.write('Please note that due to limited resources, this dataset does not include a comprehensive list of authors.')
+        author_like = st.text_input('Enter an author you like:', 'Dr. Seuss')
+        author_recommender(author_like)
 
-
-title = st.text_input('Enter the book you like:\n(please note that due to limited resources, this book dataset may not include all the books you like)', 'the very hungry caterpillar')
-
-st.set_option('deprecation.showPyplotGlobalUse', False)
-### Content Based
-st.subheader('You may also like these books:\n (content based recommendation)')  
-fig_content = content_recommender(title)
-st.pyplot(fig_content)
-
-### Collaborative Based
-st.subheader('Readers also liked these books:\n (collaborative based recommendation)')   
-fig_collab = collaborative_recommender(title)
-st.pyplot(fig_collab)
+if __name__ == '__main__':
+    main()
 
 st.write("##")
 st.write('Welcome feedback and advice, please email me: shuo.2020@outlook.com')
