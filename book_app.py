c8cb0b5959c4a926a33a320c8bdc7aaf31ea1ad5
SileiHuo <shuo.2020@outlook.com>
2022-12-30 15:52:04 -0800

    update recommendations

--- book_app.py
+++ book_app.py
@@ -1,9 +1,7 @@
 
 import pandas as pd
-#import numpy as np
 import requests
 from PIL import Image
-import matplotlib.pyplot as plt
 import streamlit as st
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
@@ -13,34 +11,42 @@ def load_image(onlineurl):
     image = Image.open(requests.get(onlineurl, stream=True).raw)
     return image
 
-
 @st.cache
 def load_data(path):
     df = pd.read_csv(path)
     return df
 
-df_book = load_data("data/children_book_processed.csv")
-df_book_item = load_data("data/children_book_collab.csv")
+@st.cache
+def load_data_index(path):
+    df = pd.read_csv(path, index_col=0)
+    return df
 
 @st.cache
 def load_matrix(path):
     matrix = sparse.load_npz(path)
     return matrix
 
+######################################################################
+### Load dataset and matrix
+df_book = load_data("data/children_book_processed.csv")
+df_book_item = load_data("data/children_book_collab.csv")
+df_svd_recommend_book = load_data_index('data/children_user_recommendation.csv')
+df_svd_toprated = load_data('data/children_user_toprated.csv')
 desfull_tfidf_matrix = load_matrix('data/desfull_tfidf_matrix.npz')
 book_user_matrix = load_matrix('data/book_user_matrix.npz')
 
+######################################################################
 ### Show entered books
 def show_book(title):
     title = str(title)
     if df_book['title'].str.contains(title, case=False).any():
         book_index = df_book[df_book['title'].str.contains(title, case=False)].sort_values(by='ratings_count',ascending=False).index[0]
         url = df_book.iloc[book_index]['image_url']
-        st.image(url, width =180, use_column_width=False, caption=df_book.iloc[book_index]['title'])
+        st.image(url, width =180, use_column_width='auto', caption=df_book.iloc[book_index]['title'])
     else:
         st.write('Sorry, book not found. Instead, below are some recommended popular books.')
 
-
+######################################################################
 ### Content Based Recommendation
 def content_recommender(title, vote_threshold=100):
     
@@ -58,28 +64,19 @@ def content_recommender(title, vote_threshold=100):
         # select top 5 books with highest similarity score
         top_books = sim_books[sim_books['rating count'] > vote_threshold].sort_values(by='similarity', ascending=False).iloc[1:6]
         return top_books
-        # plot the book cover
-        # cols= st.columns(5)
-        # for url, title, col in zip(top_books['image_url'], top_books['title'], cols[0:]):
-        #     with col:
-        #         st.image(url, use_column_width=True, caption=title)   
+        
     else:
         # if book title does not exist, recommend top rated books
         top_books = df_book[df_book['ratings_count'] > 50000].sort_values(by='average_rating', ascending=False).head(5)
         return top_books
-        # plot book cover images
-        # cols= st.columns(5)
-        # for url, title, col in zip(top_books['image_url'], pop_df['title'], cols[0:]):
-        #     with col:
-        #         st.image(url, use_column_width=True, caption=title)
-
+        
+######################################################################
 ### User-Item Collaborative Based Recommendation
 def collaborative_recommender(title, vote_threshold=100):
     
     title = str(title)
     
     if df_book_item['title'].str.contains(title, case=False).any():
-        
         book_index = df_book_item[df_book_item['title'].str.contains(title, case=False)].sort_values(by='ratings_count',ascending=False).index[0]
         # calculate cosine similarity based on user-item matrix instead
         sim_item_df = pd.DataFrame({'title': df_book_item['title'], 
@@ -87,21 +84,15 @@ def collaborative_recommender(title, vote_threshold=100):
                            'rating count': df_book_item['ratings_count'],
                            'rating': df_book_item['average_rating'],
                            'image_url': df_book_item['image_url']})
-        
         top_books = sim_item_df[sim_item_df['rating count'] > vote_threshold].sort_values(by='similarity', ascending=False).iloc[1:6]
         return top_books
-        # cols= st.columns(5)
-        # for url, title, col in zip(top_books['image_url'], top_books['title'], cols[0:]):
-        #     with col:
-        #         st.image(url, use_column_width=True, caption=title)
+       
     else:
         top_books = df_book_item[df_book_item['ratings_count'] > 50000][['title', 'average_rating', 'image_url']].sort_values(by='average_rating', ascending=False).iloc[5:10]
         return top_books
-        # cols= st.columns(5)
-        # for url, title, col in zip(pop_df['image_url'], top_books['title'], cols[0:]):
-        #     with col:
-        #         st.image(url, use_column_width=True, caption=title)
+        
 
+######################################################################
 ### Author Based Recommendation
 def author_recommender(author, vote_threshold=50):
     
@@ -117,19 +108,47 @@ def author_recommender(author, vote_threshold=50):
     else:
         st.write('Sorry, no author found by that name. Please try again.')
 
+######################################################################
+### User Based Recommendation (FunkSVD)
+def user_recommender(user):
+    user=int(user)
+    if user in df_svd_recommend_book.index:
+        book_list = list(df_svd_recommend_book.loc[user])
+        recommend_df = df_book.loc[df_book['book_id'].isin(book_list)]
+        recommend_books = recommend_df[['title', 'image_url']].sample(n=5)
+        cols= st.columns(5)
+        for url, title, col in zip(recommend_books['image_url'], recommend_books['title'], cols[0:]):
+            with col:
+                st.image(url, use_column_width=True, caption=title)
+    else:
+        st.write('Sorry, no user found by that id. Please try again.')
+
+def user_profile(user):
+    if user in df_svd_toprated['user'].unique():
+        book_list = list(df_svd_toprated[df_svd_toprated['user']==user]['book_id'])
+        toprated_books = df_book.loc[df_book['book_id'].isin(book_list)]
+        cols=st.columns(10)
+        for url, title, col in zip(toprated_books['image_url'], toprated_books['title'], cols[0:]):
+            with col:
+                st.image(url, use_column_width=True)
+    else:
+        st.write('Sorry, no user found by that id. Please try again.')
+
+######################################################################
 ### APP Presense
 st.title("Children's Book Recommendation")
 image = load_image('https://images.unsplash.com/photo-1472162072942-cd5147eb3902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80')
 st.image(image, caption='Photo by Ben White on Unsplash')
 
 def main():
-    menu = ['Search by Book Title','Search by Author']
-    choice = st.sidebar.selectbox("Choose recommendation methods below",menu)
     
-    if choice == 'Search by Book Title':
+    tab1, tab2, tab3 = st.tabs(["Search by Book Title", "Search by Author", "Recommend to Existing Users"])
+
+    with tab1:
         st.header('Recommend by Book Title')
         st.write('Please note that due to limited resources, this dataset does not include a comprehensive list of titles.')
-        book_title = st.text_input('Enter a book you like:', 'the very hungry caterpillar')
+        st.subheader('Enter a book you like:')
+        book_title = st.text_input('Enter a book you like:', 'the very hungry caterpillar', label_visibility="collapsed")
         show_book(book_title)
         
         ### Content Based
@@ -147,12 +166,24 @@ def main():
             with col:
                 st.image(url, use_column_width=True, caption=title)
     
-    else:
+    with tab2:
         ### Author Recommendation
         st.header('Recommend by Author')
         st.write('Please note that due to limited resources, this dataset does not include a comprehensive list of authors.')
-        author_like = st.text_input('Enter an author you like:', 'Dr. Seuss')
+        st.subheader('Enter an author you like:')
+        author_like = st.text_input('Enter an author you like:', 'Eric Carle', label_visibility="collapsed")
         author_recommender(author_like)
+    
+    with tab3:
+        ### User Recommendation
+        st.header('Recommend to Existing Users \n (Recommendations based on predicted rating via FunkSVD)')
+        st.write('Please note that due to limited resources, here only shows a subsample. User ID is adjusted to numbers from 1 to 3300.')
+        st.subheader('Please enter your user id:')
+        user_id = st.number_input('', min_value=1, max_value=3342, label_visibility="collapsed")
+        st.subheader('You may also like these books:')
+        user_recommender(str(user_id))
+        st.subheader('Your rated books:')
+        user_profile(user_id)
 
 if __name__ == '__main__':
     main()
